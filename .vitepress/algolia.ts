a4c74a587ef642d1fc0889c8c4907a356c7b0d7a
devlink <devlink@devlinkdeMacBook-Air.local>
2025-06-27 22:39:27 +0800

    Updates

---
+++ .vitepress/algolia.ts
@@ -0,0 +1,11 @@
+export default {
+  appId: '29MPNEIX2R',
+  apiKey: '2b961ecaa730d7dea178254c360f0cca',
+  indexName: 'chocn',
+  placeholder: '请输入关键词',
+  translations: {
+    button: {
+      buttonText: '搜索文档',
+    },
+  },
+}
